6f5a3fd0b295c930586773e64f0d64d308da3ad4
Eduardo Villota <eduardouio7@gmail.com>
2024-07-08 04:34:21 -0500

    eliminamos los titulos de la pagina

--- src/components/Breadcrumbs/BreadcrumbDefault.vue
+++ src/components/Breadcrumbs/BreadcrumbDefault.vue
@@ -3,11 +3,7 @@ const props = defineProps(['pageTitle'])
 </script>
 
 <template>
-  <div class="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-    <h2 class="text-title-md2 font-semibold text-black dark:text-white">
-      {{ props.pageTitle }}
-    </h2>
-
+  <div class="mb-3 flex flex-col justify-end  sm:flex-row sm:items-center sm:justify-between bg-slate-200">
     <nav>
       <ol class="flex items-center gap-2">
         <li>
