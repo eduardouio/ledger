d3d285f9d0ae8850b9c6fa6cdc177807248df96b
Eduardo Villota <eduardouio7@gmail.com>
2024-07-29 10:00:50 -0500

    card del home

--- front/src/components/DataStats/DataStatsOne.vue
+++ front/src/components/DataStats/DataStatsOne.vue
@@ -1,76 +1,36 @@
-<script setup lang="ts">
+<script setup>
+import { IconHomeDollar, IconClipboardData, IconUsers, IconPlus, IconList } from '@tabler/icons-vue';
 import { ref } from 'vue'
-
-const cardItems = ref([
-  {
-    icon: `
-      <svg  
-        xmlns="http://www.w3.org/2000/svg"
-        class=" text-primary dark:fill-white"  
-        width="24"
-        height="24" 
-        viewBox="0 0 24 24"
-        fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-clock">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 7v5l3 3" /></svg>
-    `,
-    title: 'Total Hours Class',
-    total: '2230 Hrs',
-  },
-  {
-    icon: `<svg  xmlns="http://www.w3.org/2000/svg"  class=" text-primary dark:fill-white"   width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-premium-rights"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M13.867 9.75c-.246 -.48 -.708 -.769 -1.2 -.75h-1.334c-.736 0 -1.333 .67 -1.333 1.5c0 .827 .597 1.499 1.333 1.499h1.334c.736 0 1.333 .671 1.333 1.5c0 .828 -.597 1.499 -1.333 1.499h-1.334c-.492 .019 -.954 -.27 -1.2 -.75" /><path d="M12 7v2" /><path d="M12 15v2" /></svg>`,
-    title: 'Total Owed July',
-    total: '$3245,20',
-  },
-  {
-    icon: `<svg  xmlns="http://www.w3.org/2000/svg"  class=" text-primary dark:fill-white"   width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-building-community"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" /><path d="M13 7l0 .01" /><path d="M17 7l0 .01" /><path d="M17 11l0 .01" /><path d="M17 15l0 .01" /></svg>`,
-    title: 'Sucursales',
-    total: '12',
-  },
-  {
-    icon: `<svg
-            class="fill-primary dark:fill-white"
-            width="22"
-            height="18"
-            viewBox="0 0 22 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.18418 8.03751C9.31543 8.03751 11.0686 6.35313 11.0686 4.25626C11.0686 2.15938 9.31543 0.475006 7.18418 0.475006C5.05293 0.475006 3.2998 2.15938 3.2998 4.25626C3.2998 6.35313 5.05293 8.03751 7.18418 8.03751ZM7.18418 2.05626C8.45605 2.05626 9.52168 3.05313 9.52168 4.29063C9.52168 5.52813 8.49043 6.52501 7.18418 6.52501C5.87793 6.52501 4.84668 5.52813 4.84668 4.29063C4.84668 3.05313 5.9123 2.05626 7.18418 2.05626Z"
-              fill=""
-            />
-            <path
-              d="M15.8124 9.6875C17.6687 9.6875 19.1468 8.24375 19.1468 6.42188C19.1468 4.6 17.6343 3.15625 15.8124 3.15625C13.9905 3.15625 12.478 4.6 12.478 6.42188C12.478 8.24375 13.9905 9.6875 15.8124 9.6875ZM15.8124 4.7375C16.8093 4.7375 17.5999 5.49375 17.5999 6.45625C17.5999 7.41875 16.8093 8.175 15.8124 8.175C14.8155 8.175 14.0249 7.41875 14.0249 6.45625C14.0249 5.49375 14.8155 4.7375 15.8124 4.7375Z"
-              fill=""
-            />
-            <path
-              d="M15.9843 10.0313H15.6749C14.6437 10.0313 13.6468 10.3406 12.7874 10.8563C11.8593 9.61876 10.3812 8.79376 8.73115 8.79376H5.67178C2.85303 8.82814 0.618652 11.0625 0.618652 13.8469V16.3219C0.618652 16.975 1.13428 17.4906 1.7874 17.4906H20.2468C20.8999 17.4906 21.4499 16.9406 21.4499 16.2875V15.4625C21.4155 12.4719 18.9749 10.0313 15.9843 10.0313ZM2.16553 15.9438V13.8469C2.16553 11.9219 3.74678 10.3406 5.67178 10.3406H8.73115C10.6562 10.3406 12.2374 11.9219 12.2374 13.8469V15.9438H2.16553V15.9438ZM19.8687 15.9438H13.7499V13.8469C13.7499 13.2969 13.6468 12.7469 13.4749 12.2313C14.0937 11.7844 14.8499 11.5781 15.6405 11.5781H15.9499C18.0812 11.5781 19.8343 13.3313 19.8343 15.4625V15.9438H19.8687Z"
-              fill=""
-            />
-          </svg>`,
-    title: 'Total Users',
-    total: '3.456',
-    growthRate: -0.95
-  }
-])
 </script>
-
 <template>
   <!-- Card Item Start -->
-  <div
-    v-for="(item, index) in cardItems"
-    :key="index"
-    class="rounded-sm border border-stroke bg-blue-200 py-3 px-10 shadow-default dark:border-strokedark dark:bg-blue-700 flex justify-between gap-2"
-  >
-    <div
-      class="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4"
-      v-html="item.icon"
-    ></div>
+  <div class="relative bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl border border-cyan-500">
+            <div class=" text-white flex items-center absolute rounded-full py-4 px-4 shadow-xl left-4 -top-6 border bg-gray-100 border-cyan-500">
+               <IconHomeDollar class="h-8 w-8 text-cyan-500"/>
+            </div>
+            <div class="mt-8">
+                <p class="text-xl font-semibold my-2">Sales</p>
+                <div class="flex space-x-2 text-sm">
+                    <IconClipboardData class="h-5 w-5 text-green-600"/>
+                     <p class="text-gray-600">Report</p> 
+                </div>
+                <div class="flex space-x-2 text-gray-400 text-sm my-3">
+                    <IconUsers class="h-5 w-5 text-green-600"/>
+                     <p class="text-gray-600">Customers</p> 
+                </div>
+                <div class="border-t-2 "></div>
 
-    <div>
-        <h4 class="text-title-md font-bold text-black dark:text-white">{{ item.total }}</h4>
-        <span class="text-sm font-medium">{{ item.title }}</span>
-    </div>
-  </div>
+                <div class="flex justify-between pt-4">
+                    <button class="btn btn-sm ">
+                      <IconPlus class="h-5 w-5 text-white"/>
+                      New
+                    </button>
+                    <button class="btn btn-sm">
+                      <IconList class="h-5 w-5 text-white"/>
+                      List
+                    </button>
+                </div>
+            </div>
+        </div>
   <!-- Card Item End -->
 </template>
