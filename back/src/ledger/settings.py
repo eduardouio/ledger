b577ef8fc292327af937115579c54a48e98945e9
Eduardo Villota <eduardouio7@gmail.com>
2024-07-28 23:45:37 -0500

    agregamos los modulos iniciales

--- back/src/ledger/settings.py
+++ back/src/ledger/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'accounts',
+    'accunting',
+    'companies',
+    'crm',
+    'inventary',
+    'invoices',
 ]
 
 MIDDLEWARE = [
