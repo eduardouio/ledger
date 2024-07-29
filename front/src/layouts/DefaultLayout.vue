9419a86c0aecbe7d00a690188efc9607466b6829
Eduardo Villota <eduardouio7@gmail.com>
2024-07-29 13:50:48 -0500

    corregimos el layout de la pnatilla para que cubra el ancho de la pantalla

--- front/src/layouts/DefaultLayout.vue
+++ front/src/layouts/DefaultLayout.vue
@@ -18,7 +18,7 @@ import SidebarArea from '@/components/Sidebar/SidebarArea.vue'
 
       <!-- ===== Main Content Start ===== -->
       <main class="bg-base-100">
-        <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+        <div class="mx-auto p-4">
           <slot></slot>
         </div>
       </main>
