8ab80ac036c1bbaf76748e7fb68d3cb37dbfe643
Eduardo Villota <eduardouio7@gmail.com>
2024-08-17 18:21:17 -0500

    archivo semillero

--- back/src/accounts/management/commands/sowseed.py
+++ back/src/accounts/management/commands/sowseed.py
@@ -1,11 +1,34 @@
 from django.core.management.base import BaseCommand
+from companies.models import Company
+from accounts.models import CustomUserModel
 
 
 class Command(BaseCommand):
     help = 'This command creates a list of users'
 
     def handle(self, *args, **options):
-        print('Cargamos las cuentas contables')
+        print('Creamos empresa base')
+        self.LoadBaseEnterprise()
+        print('Fin')
+        print('Cargamos Cuentas contables')
+        self.loadCOA()
+        print('Fin')
+
+    def LoadBaseEnterprise(self):
+        testCompanie = Company.objects.filter(
+            tax_id='9999999999'
+        )
+        if testCompanie:
+            print('Ya Existe, no se crea')
+            return True
+
+        myUser = CustomUserModel.objects.get('eduardouio7@gmail.com')
+        Company.objects.create(
+            tax_id='9999999999',
+            name='SYSTEM COMPANY',
+            address='Companie sistem for default data',
+            manager=myUser
+        )
 
     def loadCOA(self):
         pass
