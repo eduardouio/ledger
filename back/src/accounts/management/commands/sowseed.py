ea7f5fb23c9d3b3a06733a258088a58b06aaafaa
Eduardo Villota <eduardouio7@gmail.com>
2024-08-25 06:27:44 -0500

    insersion de datos iniciiales

--- back/src/accounts/management/commands/sowseed.py
+++ back/src/accounts/management/commands/sowseed.py
@@ -1,20 +1,34 @@
 from django.core.management.base import BaseCommand
 from companies.models import Company
 from accounts.models import CustomUserModel
+from accounting.models import Account
 import json
-import os
 
 
 class Command(BaseCommand):
     help = 'This command creates a list of users'
 
     def handle(self, *args, **options):
+        print('creamos el superuser')
+        self.createSuperUser()
         print('Creamos empresa base')
         self.LoadBaseEnterprise()
         print('Fin')
         print('Cargamos Cuentas contables')
         self.loadCOAInEnterpise()
         print('Fin')
+
+    def createSuperUser(self):
+        user = CustomUserModel.get('eduardouio7@gmail.com')
+        if user:
+            print('Ya existe el usuario')
+            return True
+        CustomUserModel.objects.create(
+            email='eduardouio7@gmail.com',
+            password='seguro',
+            is_staff=True,
+            is_superuser=True
+        )
 
     def LoadBaseEnterprise(self):
         testCompanie = Company.objects.filter(
@@ -32,10 +46,36 @@ class Command(BaseCommand):
         )
 
     def loadCOAInEnterpise(self):
-        import ipdb;ipdb.set_trace()
-        my_company = Company.objects.get_by_tax_id('9999999999')
+        # Obtener la compañía
+        if Account.objects.filter(company__tax_id='9999999999').count() > 0:
+            print('Ya existen cuentas')
+            return True
+
+        my_company = Company.get_by_tax_id('9999999999')
         if not my_company:
             raise Exception('No existe el cliente System Company')
 
-        file = open('common/accounts.JSON', '+r')
-        file.readlines()
+        with open('common/accounts.JSON', 'r') as file:
+            file_content = json.load(file)
+
+        if file_content is None:
+            raise Exception('No existe el archivo de cuentas')
+        # Iterar sobre los niveles y crear las cuentas
+        # Asegura que los niveles se procesen en orden
+        for level in sorted(file_content.keys()):
+            for account_data in file_content[level]:
+                parent_account = None if level == 'level_1' else Account.get_account(
+                    account_data['parent_account'], my_company
+                )
+                print('registramos {} {}'.format(level, account_data))
+                Account.objects.create(
+                    code=account_data['code'],
+                    company=my_company,
+                    name=account_data['name'],
+                    type=account_data['type'],
+                    level=account_data['level'],
+                    is_children=account_data['is_children'],
+                    description=account_data['description'],
+                    parent_account=parent_account,
+                    id_user_created=1
+                )
