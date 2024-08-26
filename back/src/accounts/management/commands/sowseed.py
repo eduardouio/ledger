0dd2cbb20ffd5d8c31f1c2ef157709dfc7a4f128
Eduardo Villota <eduardouio7@gmail.com>
2024-08-26 15:20:12 -0500

    sowseed

--- back/src/accounts/management/commands/sowseed.py
+++ back/src/accounts/management/commands/sowseed.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand
 from companies.models import Company
 from accounts.models import CustomUserModel
-from accounting.models import Account, Bank
+from accounting.models import Account, Bank, Transaction
+from invoices.models import Invoice, InvoiceItems, Payment
 from crm.models import Partner
 from inventary.models import Product
+import random
 from faker import Faker
 import json
 
@@ -26,6 +28,8 @@ class Command(BaseCommand):
         self.createPartners(faker)
         print('cargamos los productos')
         self.createProducts(faker)
+        print('cargamos los movimientos de inventario')
+        self.loadTransactions(faker)
         print('Fin')
 
     def createSuperUser(self):
@@ -184,3 +188,45 @@ class Command(BaseCommand):
                 type='service'
             )
         return True
+
+    def loadTransactions(self, faker):
+        company = Company.get_by_tax_id('9999999999')
+        products = Product.get_by_type('product', company)
+        suppliers = Partner.get_by_type('supplier', company)
+        customers = Partner.get_by_type('customer', company)
+        user = CustomUserModel.get('eduardouio7@gmail.com')
+        banks = Bank.objects.all()
+        self.generateBills(company, user, products, suppliers, faker)
+
+    def generateBills(self, company, user, products, suppliers, faker):
+        bills = Invoice.get_by_type('bill', company.name)
+        if len(bills) > 0:
+            print('Ya existen compras')
+            return True
+
+        for suppier in suppliers:
+            for _ in range(random.randint(1, 20)):
+                bill_date = faker.date_this_year()
+                due_date = faker.date_between(
+                    start_date=bill_date, end_date='+30d'
+                )
+                bill = Invoice.objects.create(
+                    company=company,
+                    partner=suppier,
+                    type='bill',
+                    date=bill_date,
+                    due_date=due_date,
+                    number=faker.ean8(),
+                    amount=faker.random_number(2),
+                    user=user,
+                    tax=12.2
+                )
+                for _ in range(random.randint(1, 10)):
+                    product = random.choice(products)
+                    InvoiceItems.objects.create(
+                        invoice=bill,
+                        product=product,
+                        quantity=random.randint(1, 100),
+                        price=product.price,
+                        discount=random.randfloat(0, 0.5)
+                    )
