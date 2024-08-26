a056be98f1f2de8a72f6f3dd10a7a81269a5d5e9
Eduardo Villota <eduardouio7@gmail.com>
2024-08-26 17:23:27 -0500

    serializaers

--- back/src/api/Serializer.py
+++ back/src/api/Serializer.py
@@ -1,2 +1,83 @@
 from rest_framework import serializers
 from accounting.models import Account, Transaction, GeneralLedger
+from companies.models import Company
+from inventary.models import Product, InventoryMovement
+from crm.models import Partner
+from invoices.models import Invoice, Payment, InvoiceItems
+from warenhouses.models import Warenhouse
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        depth = 2
+
+
+class GeneralLedgerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GeneralLedger
+        fields = '__all__'
+        depth = 2
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+        depth = 2
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        depth = 2
+
+
+class InventoryMovementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryMovement
+        fields = '__all__'
+        depth = 2
+
+
+class PartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Partner
+        fields = '__all__'
+        depth = 2
+
+
+class InvoiceItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InvoiceItems
+        fields = '__all__'
+        depth = 2
+
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+        depth = 2
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
+        depth = 2
+
+
+class WarenhouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warenhouse
+        fields = '__all__'
+        depth = 2
