d79b0908dc209a85c1dc5628d4fdaa2ab4730550
Eduardo Villota <eduardouio7@gmail.com>
2024-08-27 11:44:46 -0500

    directprio con vistas genericas

---
+++ back/src/api/views/generics/RetrieveViews.py
@@ -0,0 +1,12 @@
+from api.serializers import (AccountSerializer,
+                             TransactionSerializer,
+                             GeneralLedgerSerializer,
+                             CompanySerializer,
+                             ProductSerializer,
+                             InventoryMovementSerializer,
+                             PartnerSerializer,
+                             InvoiceItemsSerializer,
+                             InvoiceSerializer,
+                             PaymentSerializer,
+                             WarenhouseSerializer
+                             )
