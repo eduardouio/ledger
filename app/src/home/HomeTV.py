3d465d9ca07c5336de4ead23e985b842dccddfde
Eduardo Villota <eduardouio7@gmail.com>
2024-09-10 14:43:33 -0500

    creamos estructura por pagina

--- app/src/home/HomeTV.py
+++ app/src/home/HomeTV.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 # /
 class HomeTV(TemplateView):
-    template_name = 'base/home.html'
+    template_name = 'pages/home.html'
 
     def get(self, request, *args, **kwargs):
         ctx = self.get_context_data(**kwargs)
