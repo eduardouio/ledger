eda669811844d9adc809dcbdec75fa0e5fe0f9c1
Eduardo Villota <eduardouio7@gmail.com>
2024-09-10 14:51:35 -0500

    reestructuramos por paginas

--- app/src/ledger/urls.py
+++ app/src/ledger/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', include('pages.urls')),
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
--- app/src/pages/urls.py
+++ app/src/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home import HomeTV
+from pages import HomeTV
 
 
 urlpatterns = [
