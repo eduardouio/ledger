fe68f226be4f8ab83e4ad2701317c3b98267fca6
Eduardo Villota <eduardouio7@gmail.com>
2024-10-06 04:03:52 -0500

    refactor

--- app/src/invoices/views/BillsCRUD.py
+++ app/src/invoices/views/BillsCRUD.py
@@ -1,67 +1,75 @@
+import json
+from datetime import datetime
+from decimal import Decimal
 from django.urls import reverse_lazy
+from django.http import JsonResponse
 from django.views.generic import (
-    CreateView,
-    UpdateView,
+    TemplateView,
     DeleteView,
     ListView,
     DetailView
 )
-from django.forms.models import inlineformset_factory
+from django.core.serializers import serialize
+from django.contrib.auth.mixins import LoginRequiredMixin
 from invoices.models import Invoice, InvoiceItems
-from invoices.forms import InvoiceForm, InvoiceItemsForm
+from crm.models import Partner
 from companies.models import Company
 from inventary.models import Product
-from django.core.serializers import serialize
 
 
-InvoiceItemFormSet = inlineformset_factory(
-    Invoice, InvoiceItems,
-    form=InvoiceItemsForm,
-    extra=20
-)
-
-
-class BillCreateView(CreateView):
+class BillCreateView(LoginRequiredMixin, TemplateView):
     model = Invoice
-    form_class = InvoiceForm
-    template_name = 'bills/bills-form.html'
-    success_url = reverse_lazy('bills-list')
+    template_name = 'bills/bill-form.html'
 
     def get_context_data(self, **kwargs):
         ctx = super(BillCreateView, self).get_context_data(**kwargs)
         company = Company.get_by_user(self.request.user)
         ctx['company'] = company
+        products = Product.get_all(ctx['company'])
+        partners = Partner.get_suppliers(ctx['company'])
         ctx['title_bar'] = 'Create Bill Invoice'
-        products =  Product.get_all(company)
         ctx['products'] = serialize('json', products)
+        ctx['suppliers'] = serialize('json', partners)
+        ctx['company_data'] = serialize('json', [company])
         return ctx
 
-    def form_valid(self, form):
-        context = self.get_context_data()
-        formset = context['formset']
-        if formset.is_valid():
-            self.object = form.save()
-            formset.instance = self.object
-            formset.save()
-            return super().form_valid(form)
-        else:
-            return self.form_invalid(form)
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        invoice_data = data['invoice_headers']
+        line_items = data['invoice_items']
+        company = Company.get_by_user(request.user)
+        supplier = Partner.get_by_id(invoice_data['supplier']['id'])
+        invoice = Invoice.objects.create(
+            company=company,
+            partner=supplier,
+            type='BILL',
+            date=datetime.fromisoformat(invoice_data['date']),
+            due_date=datetime.fromisoformat(invoice_data['due_date']),
+            number=invoice_data['number'],
+            amount=Decimal(invoice_data['total']),
+            tax=Decimal(invoice_data['tax']),
+            discount=Decimal(invoice_data['discount']),
+            pay_terms=invoice_data['pay_terms'],
+            status='ACEPTED',
+            user=request.user
+        )
 
-
-class BillDeleteView(DeleteView):
-    model = Invoice
-    template_name = 'bills/bills-confirm-delete.html'
-    success_url = reverse_lazy('invoice-list')
-
-    def get_context_data(self, **kwargs):
-        ctx = super(BillDeleteView, self).get_context_data(**kwargs)
-        ctx['title_bar'] = 'Delete Invoice'
-        return ctx
+        for item in line_items:
+            product = Product.get_by_id(item['product']['id'], company)
+            InvoiceItems.objects.create(
+                invoice=invoice,
+                product=product,
+                quantity=item['quantity'],
+                price=item['price'],
+                discount=item['discount']
+            )
+            url = reverse_lazy('bill-detail', kwargs={'pk': invoice.id})
+            return JsonResponse({'url': url, 'status': 'ok'}, satatus=201)
 
 
 class BillListView(ListView):
     model = Invoice
-    template_name = 'bills/bills-list.html'
+    template_name = 'bills/bill-list.html'
     context_object_name = 'invoices'
 
     def get_queryset(self):
@@ -84,5 +92,28 @@ class BillDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         ctx = super(BillDetailView, self).get_context_data(**kwargs)
+        invoice_items = []
+        subtotal_1 = 0
+        total_discount = 0
+        total = 0
+
+        for item in InvoiceItems.get_by_invoice(self.object):
+            total += (item.price * item.quantity) - item.discount
+            subtotal_1 += item.price * item.quantity
+            total_discount += item.discount
+            invoice_items.append({
+                'product': item.product,
+                'quantity': item.quantity,
+                'price': item.price,
+                'discount': item.discount,
+                'total': (item.price * item.quantity) - item.discount
+            })
+        ctx['invoice_items'] = invoice_items
         ctx['title_bar'] = 'Invoice Detail'
+        ctx['total'] = {
+            'subtotal': subtotal_1,
+            'discount': total_discount,
+            'total': total
+        }
+        ctx['url_new'] = reverse_lazy('bills-create')
         return ctx
---
+++ app/src/static/js/app-bills.js
@@ -0,0 +1,195 @@
+
+const app = Vue.createApp({
+  // Datos reactivos
+  data() {
+    return {
+      message: '',
+      confirm_delete:false,
+      customers: supliersData.map((itm) => {
+        return {
+          id: itm.pk,
+          ...itm.fields
+        }
+      }),
+      products: productsData.map((itm) => {
+        return {
+          id: itm.pk,
+          ...itm.fields
+        }
+      }),
+      company: companyData.map((itm) => {
+        return {
+          id: itm.pk,
+          ...itm.fields
+        }
+      })[0],
+      invoice_headers: {
+        customer:null,
+        date: null,
+        pay_terms: null,
+        due_date: null,
+        number: null,
+        status: null,
+        subtotal_1: 0,
+        subtotal_2:0,
+        discount:0,
+        tax:0,
+        total:0
+      },
+      invoice_items: [],
+      new_item: {
+        product: null,
+        id:null,
+        quantity: 0,
+        price: 0,
+        discount: 0,
+        delete: false
+      }
+    };
+  },
+  // Métodos de la app
+  methods: {
+    addNewItem() {
+      if (this.new_item.product == null || this.new_item.quantity == 0){
+        this.message = 'Product/Service es required and Quantity must be greater than 0';
+        return;
+      }
+      if (this.new_item.discount == '' || this.new_item.discount == null){ 
+        this.new_item.discount = 0;
+      }
+      this.invoice_items.push({...this.new_item});
+      this.new_item = {
+        product: null,
+        quantity: 0,
+        price: 0,
+        discount: 0
+      };
+      this.is_disabled_add_item = true
+      this.$refs.quantityInput.focus();
+      this.message = ''
+    },
+    selectItem(){
+      this.new_item.price = this.new_item.product.price;
+      this.new_item.id = this.new_item.product.id;
+      this.new_item.quantity = 1;
+      this.is_disabled_add_item = false;
+    },
+    setCustomer(){
+        let today = new Date();
+        let year = today.getFullYear();
+        let month = String(today.getMonth() + 1).padStart(2, '0');
+        let day = String(today.getDate()).padStart(2, '0');
+        this.invoice_headers.date = `${year}-${month}-${day}`;
+        let days = this.invoice_headers.customer.pay_terms ?? 0; 
+        this.invoice_headers.pay_terms = days;
+        if (days == 0){
+            console.log('Cliente de contado')
+            this.invoice_headers.due_date = `${year}-${month}-${day}`;
+            return;
+        }
+        today.setDate(today.getDate() + days);
+        let due_year = today.getFullYear();
+        let due_month = String(today.getMonth() + 1).padStart(2, '0');
+        let due_day = String(today.getDate()).padStart(2, '0');
+        this.invoice_headers.due_date = `${due_year}-${due_month}-${due_day}`;
+    },
+    setDueDate(){
+      let start_date = new Date(this.invoice_headers.date);
+      let days = this.invoice_headers.pay_terms;
+      start_date.setDate(start_date.getDate() + days);
+      let due_year = start_date.getFullYear();
+      let due_month = String(start_date.getMonth() + 1).padStart(2, '0');
+      let due_day = String(start_date.getDate()).padStart(2, '0');
+      this.invoice_headers.due_date = `${due_year}-${due_month}-${due_day}`;
+    },
+    deleteItem(index){
+      this.invoice_items.splice(index, 1);
+    },
+    saveInvoice(){
+      let date = new Date(this.invoice_headers.date);
+      let due_date = new Date(this.invoice_headers.due_date);
+      if (date > due_date){
+        this.message = 'Due date must be greater than date';
+        return;
+      }
+
+      this.invoice_headers.date = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+      this.invoice_headers.due_date = `${due_date.getFullYear()}-${String(due_date.getMonth() + 1).padStart(2, '0')}-${String(due_date.getDate()).padStart(2, '0')}`;
+
+      let data = {
+        invoice_headers: this.invoice_headers,
+        invoice_items: this.invoice_items
+      };
+      console.log(data);
+      fetch('/sales/add/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status == 'ok'){
+          window.location.href = data.url;
+        }
+      })
+      .catch((error) => {
+        alert('Error has ocurred');
+        console.error('Error:', error);
+      });
+    }
+  },
+  // Ciclo de vida de la app  
+  mounted() {
+    console.log('App mounted!');
+  },
+  // Propiedades computadas
+  computed: {
+    new_item_total() {
+      return (
+        this.new_item.quantity * this.new_item.price) 
+        - this.new_item.discount;
+    },
+    subtotal_1() {
+      let subtotal_1 = this.invoice_items.reduce((acc, item) => {
+        return acc + (item.quantity * item.price);
+      },0);
+      subtotal_1 = parseFloat(subtotal_1);
+      this.invoice_headers.subtotal_1 = subtotal_1;
+      return subtotal_1.toFixed(2);
+    },
+    discount() {
+      let discount = this.invoice_items.reduce((acc, item) => {
+        return acc + (item.discount * 1 );
+      }, 0);
+      discount = parseFloat(discount);
+      this.invoice_headers.discount = discount;
+      return discount.toFixed(2);
+
+    },
+    subtotal_2() {
+      let subtotal_2 = this.subtotal_1 - this.discount;
+      return subtotal_2.toFixed(2);
+    },
+    tax() {
+      let tax = this.subtotal_2 * this.company.tax_in_sales / 100;
+      this.invoice_headers.tax = tax
+      return tax.toFixed(2);
+    },
+    total() {
+      let my_total = parseFloat(this.subtotal_2) + parseFloat(this.tax);
+      this.invoice_headers.total = my_total;
+      return my_total.toFixed(2);
+    },
+    isCompleteInvoice() {
+      return this.invoice_headers.customer 
+            && this.invoice_headers.date 
+            && this.invoice_headers.due_date
+            && this.invoice_items.length > 0;
+    },
+}});
+
+app.config.compilerOptions.delimiters = ['[[', ']]'];
+const vm = app.mount('#app');
